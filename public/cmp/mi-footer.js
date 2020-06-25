a6ff84cb2f079e019d122e494cec92ac68b6eea6
van123782 <eldadavan@gmail.com>
2020-06-25 02:15:18 -0500

    ya probe el chat

---
+++ public/cmp/mi-footer.js
@@ -0,0 +1,20 @@
+class MiFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = /* html */
+           
+           '<h3> Copyright &copy; 2020 Garcia Hurtado Daniel Giovanny <br>Copyright &copy; 2020 Sanchez Ramirez Itamar Victor<br>Copyright &copy; 2020 Vazquez Cisneros Elda Vanessa<br>Copyright &copy; 2020 Vazquez Ramirez Roberto Fabian';
+      
+           
+          
+           
+          
+          
+        
+         
+           
+         
+            
+           
+    }
+}
+customElements.define("mi-footer", MiFooter);
